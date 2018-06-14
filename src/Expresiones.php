c2f51a86e9b6c00e37cc728611d06b8a65e6bab3
osorcom <oscar.sor.com@gmail.com>
2018-06-14 07:54:33 +0200

    código de partida

---
+++ src/Expresiones.php
@@ -0,0 +1,18 @@
+<?php
+  namespace lib\utils;
+
+
+  class Expresiones {
+
+    public static function secuenciaParentesisBalanceada($cad){
+      $cars = str_split($cad);
+      $cont = 0;
+      foreach($cars as $c){
+        if($c==="(") $cont++;
+        if($c===")") $cont--;
+      }
+      return ($cont===0);
+    }
+  }
+
+?>
